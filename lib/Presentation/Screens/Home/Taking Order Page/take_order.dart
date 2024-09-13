a99daa67b4235ae7bcfa1944a38857ad673111e6
H4r1 <harigovindappu4926@gmail.com>
2024-09-13 11:40:21 +0530

    Modified the page

--- lib/Presentation/Screens/Home/Taking Order Page/take_order.dart
+++ lib/Presentation/Screens/Home/Taking Order Page/take_order.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shopping_app_delivery/Core/colors.dart';
 
 class TakeOrderPage extends StatelessWidget {
-  const TakeOrderPage({super.key});
+  TakeOrderPage({super.key});
+
+  final TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +13,37 @@ class TakeOrderPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: kwhite,
         surfaceTintColor: kwhite,
+        title: Text(
+          'Take the order',
+          style:
+              GoogleFonts.montserrat(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: SearchBar(
+              controller: searchController,
+              elevation: const WidgetStatePropertyAll(0),
+              backgroundColor: const WidgetStatePropertyAll(klightGrey),
+              hintText: 'Search with code',
+              trailing: const [
+                CircleAvatar(
+                  backgroundColor: kdarkcolor3,
+                  child: Icon(
+                    Icons.search_outlined,
+                    color: kwhite,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const Expanded(
+              child: Center(
+            child: Text('Enter The code'),
+          ))
+        ],
       ),
     );
   }
