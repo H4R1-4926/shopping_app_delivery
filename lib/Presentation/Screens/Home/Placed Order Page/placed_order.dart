3789b50a46a141d68d555ca3217cadab701755da
H4r1 <harigovindappu4926@gmail.com>
2024-09-13 11:23:02 +0530

    created new two screens

---
+++ lib/Presentation/Screens/Home/Placed Order Page/placed_order.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app_delivery/Core/colors.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kwhite,
+        surfaceTintColor: kwhite,
+      ),
+    );
+  }
+}
